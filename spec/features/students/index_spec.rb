9b94c1516f4be81ef642d774af4eff25b83d4a77
John Travers <johnk.travers@gmail.com>
2019-10-20 18:05:04 -0600

    Add test that clicking an instructor name redirects to their show page

--- spec/features/students/index_spec.rb
+++ spec/features/students/index_spec.rb
@@ -2,32 +2,44 @@ require 'rails_helper'
 
 RSpec.describe 'students index page', type: :feature do
 
+  before :each do
+    @barb = Student.create(name: 'Barb Schicke', cohort_num: '1906', age: 32)
+    @nick = Student.create(name: 'Nicholas Piere', cohort_num: '1904', age: 27)
+
+    @erin = Instructor.create(name: 'Erin Hampton')
+    @greg = Instructor.create(name: 'Greg Davis')
+    @rose = Instructor.create(name: 'Rose Sanders')
+
+    InstructorStudent.create(instructor_id: @erin.id, student_id: @nick.id)
+    InstructorStudent.create(instructor_id: @greg.id, student_id: @barb.id)
+    InstructorStudent.create(instructor_id: @rose.id, student_id: @barb.id)
+    InstructorStudent.create(instructor_id: @rose.id, student_id: @nick.id)
+  end
+
   it 'can see all students and their name, cohort, and instructors' do
-    barb = Student.create(name: 'Barb Schicke', cohort_num: '1906', age: 32)
-    nick = Student.create(name: 'Nicholas Piere', cohort_num: '1904', age: 27)
-
-    erin = Instructor.create(name: 'Erin Hampton')
-    greg = Instructor.create(name: 'Greg Davis')
-    rose = Instructor.create(name: 'Rose Sanders')
-
-    InstructorStudent.create(instructor_id: erin.id, student_id: nick.id)
-    InstructorStudent.create(instructor_id: greg.id, student_id: barb.id)
-    InstructorStudent.create(instructor_id: rose.id, student_id: barb.id)
-    InstructorStudent.create(instructor_id: rose.id, student_id: nick.id)
-
     visit '/students'
 
-    within "#student-#{barb.id}" do
-      expect(page).to have_content(barb.name)
-      expect(page).to have_content("Cohort: #{barb.cohort_num}")
-      expect(page).to have_content("Instructors:\n#{greg.name}\n#{rose.name}")
+    within "#student-#{@barb.id}" do
+      expect(page).to have_content(@barb.name)
+      expect(page).to have_content("Cohort: #{@barb.cohort_num}")
+      expect(page).to have_content("Instructors:\n#{@greg.name}\n#{@rose.name}")
     end
 
-    within "#student-#{nick.id}" do
-      expect(page).to have_content(nick.name)
-      expect(page).to have_content("Cohort: #{nick.cohort_num}")
-      expect(page).to have_content("Instructors:\n#{erin.name}\n#{rose.name}")
+    within "#student-#{@nick.id}" do
+      expect(page).to have_content(@nick.name)
+      expect(page).to have_content("Cohort: #{@nick.cohort_num}")
+      expect(page).to have_content("Instructors:\n#{@erin.name}\n#{@rose.name}")
     end
+  end
+
+  it 'can click an instructors name to go to their show page' do
+    visit '/students'
+    within("#student-#{@barb.id}") { click_link(@greg.name) }
+    expect(current_path).to eq("/instructors/#{@greg.id}")
+
+    visit '/students'
+    within("#student-#{@nick.id}") { click_link(@erin.name) }
+    expect(current_path).to eq("/instructors/#{@erin.id}")
   end
 
 end
