5f8fa5d97fdeb4b1ecd1563c5c26977066aa24e3
John Travers <johnk.travers@gmail.com>
2019-10-20 20:34:00 -0600

    Add seed data for local server testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+barb = Student.create(name: 'Barb Schicke', cohort_num: '1906', age: 32)
+nick = Student.create(name: 'Nicholas Piere', cohort_num: '1904', age: 27)
+kyle = Student.create(name: 'Kyle Zeigle', cohort_num: '1901', age: 23)
+
+erin = Instructor.create(name: 'Erin Hampton')
+greg = Instructor.create(name: 'Greg Davis')
+rose = Instructor.create(name: 'Rose Sanders')
+
+InstructorStudent.create(instructor_id: erin.id, student_id: nick.id)
+InstructorStudent.create(instructor_id: erin.id, student_id: kyle.id)
+InstructorStudent.create(instructor_id: greg.id, student_id: barb.id)
+InstructorStudent.create(instructor_id: greg.id, student_id: kyle.id)
+InstructorStudent.create(instructor_id: rose.id, student_id: barb.id)
+InstructorStudent.create(instructor_id: rose.id, student_id: nick.id)
